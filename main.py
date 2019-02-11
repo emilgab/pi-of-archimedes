bd03011bc218e246601eecd352de3230e737d90d
EmilG <10610649+emilgab@users.noreply.github.com>
2019-02-11 23:37:51 +0100

    Updated the main .py-file
    Added comments for clarification

--- main.py
+++ main.py
@@ -8,13 +8,21 @@
 
 def estimate_pi():
     
+    """
+    A function which iterates through a list of Polygons (with sides ranging from 6 to 384) and returns the estimate of pi using Archimedes' method.
+    """
+    
+    # Our polygons where the sides are the double of the previous polygon.
     polygon_sides = [6, 12, 24, 48, 96, 192, 384]
     
+    # Length of the side (radius of the circle), starts at 1 with a polygon of 6 sides.
     side_len = 1
     
     for polygon in polygon_sides:
         
         side_half = side_len/2
+        
+        # Pythagorean theorem
         a = (1-(side_half**2))**0.5
         b = 1 - a
         
@@ -24,6 +32,7 @@ def estimate_pi():
         
         print(f"Estimated Pi with a polygon of {polygon} is {estimate_pi}")
         
+        # Changes the side_len variable for our next estimate of pi, with double the amount of sides.
         side_len = ((b**2)+(side_half**2))**0.5
         
 print(estimate_pi())
